521fdd8e77d8b950e89043e2914034a7ec6104ac
minh-p <minhisverynormal@gmail.com>
2021-09-05 17:20:42 -0700

    Everything is in

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple (concept) Number Obfuscator Explaination
+1. Takes the number obfuscated and the limit.
+2. randomizing addition + negative that will add up the number obfuscated.
+3. On the last iteration (i == limit): total sum has to add up to the number obfuscated.
---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int mathSign(int num) {
+    if (num > 0) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+int* obfuscate(int num, int obfLimit) {
+    int newNum = 0;
+    int currentSum = 0;
+
+    int obfArray[obfLimit];
+    for (int i = 0; i <= obfLimit; i++) {
+        if (newNum >= num) {
+            break;
+        }
+
+        if (i < obfLimit) {
+            // randomize 0 to allowed max addition
+            newNum = rand() % (num - currentSum);
+
+            // replace randomize newNum: 0 with negative randomized number.
+            if (newNum == 0) {
+                newNum = -(rand() % (currentSum - 0));
+            }
+        } else {
+            // must add up to num now.
+            newNum = num - currentSum;
+        }
+        currentSum = currentSum + newNum;
+        obfArray[i] = newNum;
+    }
+
+    int *obfPointer = obfArray;
+    return obfPointer;
+}
+
+int main() {
+
+    // random config
+    time_t t;
+    srand((unsigned) time(&t));
+
+    int num = 10;
+    int obfLimit = 10;
+
+    int *obfuscated;
+    obfuscated = obfuscate(num, obfLimit);
+
+    for (int i = 0; i <= obfLimit; i++) {
+        printf("%d\n", obfuscated[i]);
+    }
+
+    return 0;
+}
